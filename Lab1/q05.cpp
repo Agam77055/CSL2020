6ab95ca5d72cffdef3531f6b92e68a0cd15d9327
Agam77055 <136053043+Agam77055@users.noreply.github.com>
2024-08-17 00:29:59 +0530

    Create q05.cpp

---
+++ Lab1/q05.cpp
@@ -0,0 +1,72 @@
+// Write a  program (not function) to find the set exclusive-or of two arrays.
+// Note: set exclusive-or of two arrays is the sorted 1D array of unique values that are in only one of the input arrays.
+// Both arrays has equal size.
+
+// Input Format: 
+// 1. The first line contains an integer N, the size of the arrays.
+// 2. The second line contains N space-separated integers for the first array.
+// 3. The third line contains N space-separated integers for the second array.
+
+// Output Format: 
+// A single line containing the space-separated integers of the symmetric difference (set-exclusive or) of the two arrays. 
+// The integers should be sorted in ascending order.
+
+// Sample Input: 
+// 4
+// 1 2 3 4
+// 2 3 4 5
+// Sample Output:
+// 1 5
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define FOR(i, size) for(int i = 0; i < size; i++)
+#define INF INT_MIN
+
+int main(){
+    int size1; cin>>size1;
+	int size2 = size1;
+	vector<int> A(size1);
+	vector<int> B(size2);
+
+	for (int i = 0; i < size1; i++) cin>>A[i];
+    for (int i = 0; i < size2; i++) cin>>B[i];
+
+    int m = INF;
+    FOR(i, size1)
+    {
+        if (m < A[i])
+        {
+            m = A[i];
+        }
+        
+        if (m < B[i])
+        {
+            m = B[i];
+        }
+    }
+    
+    vector<int> hash(m + 1, 0);
+    
+    FOR(i, size1)
+    {
+        hash[A[i]]++;
+        hash[B[i]]++;
+    }
+    
+    FOR(i, m + 1)
+    {
+        if (hash[i] == 1)
+        {
+            cout << i << " ";
+        }
+    }
+
+    //write your code here
+    
+	
+    
+    return 0;
+}
